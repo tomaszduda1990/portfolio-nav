64bd381474637e1b168b285273554df2f79152e7
Tomasz Duda <tomaszduda1990@users.noreply.github.com>
2019-06-05 17:04:58 +0200

    adding js to about me

---
+++ src/scripts/aboutMe.js
@@ -0,0 +1,9 @@
+export class {
+  constructor(){
+    this.currentSlide = 1;
+  }
+}
+
+// add on touch start / end and on mouse down / up events
+// make a global X variable which will controll x move of h3 and p
+// if X will be too big change currentSlide
